05830ccce06a42bad45039d8125c515c14cb0f88
Jan <20224914+F3neden@users.noreply.github.com>
2018-10-15 18:20:48 +0200

    initial commit
    first basic basis for upcoming tasks + solutions

---
+++ hackerrank/src/hackerrank/Hackerrank.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hackerrank;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Benutzer1
+ */
+public class Hackerrank {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Hackerrank h = new Hackerrank();
+        Solution loesung = new Solution();
+        // initialize your new task here
+    }
+    
+    // create the task inside a new method here
+}
+
---
+++ hackerrank/src/hackerrank/Solution.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hackerrank;
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+import jdk.nashorn.internal.objects.NativeArray;
+/**
+ *
+ * @author Benutzer1
+ */
+public class Solution {
+    
+    public Solution() {
+    }
+
+    // create a new Method for each soliution inside this class
+}
