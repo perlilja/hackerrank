1be082b47e8b104a516ef47aa207165333245b5f
Jan <20224914+F3neden@users.noreply.github.com>
2018-10-15 19:10:37 +0200

    added attempt
    this is not yet working for all three test cases.
    
    It does not recognize the local maxima yet!

--- hackerrank/src/hackerrank/Solution.java
+++ hackerrank/src/hackerrank/Solution.java
@@ -45,7 +45,62 @@ public class Solution {
     
     public int stockMaximize(int[] prices) {
         int result = 0;
+        int max = 0;
+        ArrayList<Integer> gekauft = new ArrayList();
+        System.out.println("stockMaximize wird aufgerufen \n");
+        for(int i = 0; i<prices.length; i++) {
+            for(int j = i; j<prices.length; j++) {
+                if(prices[i]<prices[j])
+                {
+                    // Kaufen 
+                    if(!gekauft.contains(prices[i])) {
+                        System.out.println( "bei " + prices[i] +" wird gekauft, weil " + prices[j] + " groeßer ist." );
+                        gekauft.add(prices[i]);
+                    }
+                    else {
+                        System.out.println("bei " + prices[i] + " wurde bereits gekauft!");
+                    }
+                    
+                    // Maximum / Maxima finden
+                    if (prices[j] > max)
+                    {
+                        max = prices[j];
+                    }
+                    
+                    System.out.println("");
+                }
+                
+                // vielleicht nur den nächsten wert also i+1 betrachten?
+                if(gekauft.size() > 0 && prices[j] < prices[i])
+                {   
+                    // derzeitiger Wert lokales oder globales Maximum?
+                    System.out.println( prices[i] + " als Maximum gefunden!");
+                    
+                    for(int k = 0; k<prices.length; k++) {
+                        if (prices[j] > max)
+                    {
+                        max = prices[j];
+                    }
+                    }
+                    
+                    // if global
+                    // alles verkaufen
+                    // von neu starten
+                    
+                    // if lokal
+                    // kaufen
+                }
+            }
+        }
         
+        System.out.println("Das Maximum ist: " + max);
+        
+        
+        // Verkaufen
+        for(int k = 0; k < gekauft.size(); k++)
+        {
+            result = result + (max - gekauft.get(k));
+        }
         return result;
     }
 }
